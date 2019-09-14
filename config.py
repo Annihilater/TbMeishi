a16bb2da20051ac9344a63b246d18c3d0b2bf893
Annihilater <yanmiexingkong@gmail.com>
2019-09-15 02:42:00 +0800

    chore: update config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2019-09-08 11:31
+# @Author: PythonVampire
+# @email : vampire@ivamp.cn
+# @File  : config.py
+
+taobao_login_url = 'https://login.taobao.com/member/login.jhtml?' \
+                   'spm=a21bo.2017.754894437.1.5af911d9n3dViV&f=top&redirectURL=https%3A%2F%2Fwww.taobao.com%2F'
+
+KEYWORD = '美食'
